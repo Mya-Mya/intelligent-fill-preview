d2ddc8753ed9a70c24cf45e49f4c5e39f9d09331
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-07 10:11:33 +0900

    Format

--- tkfrontend.py
+++ tkfrontend.py
@@ -1,23 +1,24 @@
 import tkinter as tk
-from tkinter import Tk, Frame, Canvas, Label,Button,filedialog
+from tkinter import Tk, Frame, Canvas, Label, Button, filedialog
 from PIL import ImageTk, Image
 
 _imagetk_list = []
 
+
 class MenuBar(Frame):
     def __init__(self, master):
         super().__init__(master)
-        label = Label(self,text="Intelligent Fill Preview")
-        label.grid(column=0,row=0)
-        open_button = Button(self,text="Open File",command=self.on_open_button_press)
-        open_button.grid(column=1,row=0)
-    
+        label = Label(self, text="Intelligent Fill Preview")
+        label.grid(column=0, row=0)
+        open_button = Button(self, text="Open File", command=self.on_open_button_press)
+        open_button.grid(column=1, row=0)
+
     def on_open_button_press(self):
-        fn = filedialog.askopenfilename(filetypes=[("Image","*")])
-        if fn is None or fn=="":
+        fn = filedialog.askopenfilename(filetypes=[("Image", "*")])
+        if fn is None or fn == "":
             return
-        
-        
+
+
 class PictureCanvas(Canvas):
     def __init__(self, master):
         super().__init__(master)
@@ -75,10 +76,11 @@ class MainWindow(Tk):
         self.geometry("400x400")
         MenuBar(self).pack()
         picture_canvas = PictureCanvas(self)
-        image:Image.Image = Image.open("sample/sample1.png")
+        image: Image.Image = Image.open("sample/Original.png")
         picture_canvas.pack(expand=True, fil=tk.BOTH)
         picture_canvas.set_image(image)
 
 
 if __name__ == "__main__":
     main_window = MainWindow()
+    main_window.mainloop()
