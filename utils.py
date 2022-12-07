5e6df2078e65448c7c3fa3daade65d054b0f3ab7
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-07 14:44:53 +0900

    Directly Import

--- utils.py
+++ utils.py
@@ -1,19 +1,19 @@
-from PIL import Image
-import cv2
-import numpy as np
+from PIL.Image import fromarray, composite, Image
+from numpy import asarray, any
 
 
+def replace_changed_region(dst: Image, before: Image, after: Image) -> Image:
     """
     Extract the changed parts from `after` compared to `before`. Then override them to `dst`.
     """
 
     before = before.convert("RGB")
     after = after.convert("RGB")
-    b = np.asarray(before)
-    a = np.asarray(after)
+    b = asarray(before)
+    a = asarray(after)
     delta = a - b
-    changed = np.any(delta, axis=2)
-    mask = Image.fromarray(changed).convert("L")
+    changed = any(delta, axis=2)
+    mask = fromarray(changed).convert("L")
     dst = dst.convert("RGB")
-    replaced = Image.composite(after,dst,mask)
+    replaced = composite(after, dst, mask)
     return replaced
