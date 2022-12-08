2282c6d551bb4056d07711a7f29b5d68ff5d2a58
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2022-12-08 10:18:11 +0900

    Add cache

--- intelligent_filler.py
+++ intelligent_filler.py
@@ -3,8 +3,9 @@ from normal_filler import NormalFiller
 from utils import replace_changed_region
 from PIL.Image import Image, fromarray
 from pathlib import Path
-from tensorflow import keras,Tensor
-from numpy import ones, ndarray, asarray,cast,int32
+from tensorflow import keras, Tensor
+from numpy import ones, ndarray, asarray, cast, int32
+from hashlib import sha1
 
 
 class IntelligentFiller(Filler):
@@ -12,23 +13,33 @@ class IntelligentFiller(Filler):
         super().__init__()
         self.model = keras.models.load_model(model_path)
         self.normal_filler = NormalFiller()
+        self.line_closed_image_cache = {}
 
-    def fill(
-        self, image: Image, x: int, y: int, r: int, g: int, b: int, diff: int
-    ) -> Image:
+    def close_line(self, image: Image) -> Image:
+        image_hash = sha1(image.tobytes())
+        if image_hash in self.line_closed_image_cache:
+            return self.line_closed_image_cache[image_hash]
+
         width = image.width
         height = image.height
-        # Run Line Close Model
         input_image_mono = image.convert("L").point(
             lambda x: int(x > 200) * 255, mode="1"
         )
         input_image_np: ndarray = ones((height, width)) * input_image_mono
         model_input = input_image_np.reshape(1, height, width, 1)
-        model_output:Tensor = self.model(model_input)
+        model_output: Tensor = self.model(model_input)
         output_height, output_width = model_output.shape[1:3]
         model_output_np = asarray(model_output).reshape(output_height, output_width)
-        line_closed_image_np = cast[int32](model_output_np*255.)
+        line_closed_image_np = cast[int32](model_output_np * 255.0)
         line_closed_image = fromarray(line_closed_image_np)
+
+        self.line_closed_image_cache[image_hash] = line_closed_image
+        return line_closed_image
+
+    def fill(
+        self, image: Image, x: int, y: int, r: int, g: int, b: int, diff: int
+    ) -> Image:
+        line_closed_image = self.close_line(image)
 
         # Fill Line Closed Image
         filled_lined_closed_image = self.normal_filler.fill(
