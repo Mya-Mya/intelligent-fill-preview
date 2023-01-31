eb9d6666ecc385ef2060aa18b877d1d661586086
Mya-Mya <39019907+Mya-Mya@users.noreply.github.com>
2023-01-31 23:29:20 +0900

    Rebuilt by MVP design

--- tkfrontend.py
+++ tkfrontend.py
@@ -1,129 +1,127 @@
-import tkinter as tk
-from tkinter import Tk, Frame, Canvas, Label, Button, filedialog, Entry
-from PIL import ImageTk, Image
-from intelligent_filler import IntelligentFiller
+from ifrontend import IFrontend
+from presenter import Presenter
+from iftypes import Color, convert_color_to_hexcolor
+from PIL import Image
 from pathlib import Path
+from tkinter import (
+    filedialog,
+    messagebox,
+    colorchooser,
+    Tk,
+    Frame,
+    Button,
+    IntVar,
+    Radiobutton,
+    Scale,
+    BOTH,
+    LEFT,
+    RIGHT,
+    HORIZONTAL,
+)
+from imagecanvas import ImageCanvas
 
-filler = IntelligentFiller(Path("./models/model1.h5"))
+class TKFrontend(Tk, IFrontend):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title("Intelligent Fill Preview")
+        self.geometry("600x600")
+        self.init_components()
+        self.init_presenter()
+        self.mainloop()
 
-_imagetk_list = []
+    def init_components(self)->None:
+        # Image Canvas
+        self.image_canvas = ImageCanvas(self, lambda x, y: self.on_click(x, y))
+        self.image_canvas.pack(expand=True, fill=BOTH)
 
-
-class MenuBar(Frame):
-    def __init__(self, master, set_image):
-        super().__init__(master)
-        self.set_image = set_image
-        open_button = Button(self, text="Open File", command=self.on_open_button_press)
-        open_button.grid(column=0, row=0)
-
-        def validate_color_power(x):
-            return 0 <= int(x) <= 255
-
-        validate_color_power_command = self.register(validate_color_power)
-
-        Label(self, text="R").grid(column=1, row=0)
-        self.r_input = Entry(
-            self, validate="key", validatecommand=(validate_color_power_command, "%P")
+        # Menu Bar
+        menubar = Frame(self)
+        # Undo Button < Menu Bar
+        self.undo_button = Button(
+            menubar, text="Undo", command=lambda: self.presenter.on_undo_press()
         )
-        self.r_input.grid(column=1, row=1)
-        Label(self, text="G").grid(column=2, row=0)
-        self.g_input = Entry(
-            self, validate="key", validatecommand=(validate_color_power_command, "%P")
+        self.undo_button.pack(side=LEFT)
+        # Open Image Button < Menu Bar
+        openimage_button = Button(
+            menubar,
+            text="Open",
+            command=lambda: self.presenter.on_open_imagefile_press(),
         )
-        self.g_input.grid(column=2, row=1)
-        Label(self, text="B").grid(column=3, row=0)
-        self.b_input = Entry(
-            self, validate="key", validatecommand=(validate_color_power_command, "%P")
+        openimage_button.pack(side=LEFT)
+        # Fill Color Button < Menu Bar
+        self.fillcolor_button = Button(
+            menubar,
+            text="",
+            command=lambda: self.presenter.on_open_fillcolor_press(),
+            width=10,
         )
-        self.b_input.grid(column=3, row=1)
+        self.fillcolor_button.pack(side=LEFT)
+        # Diff Scale < Menu Bar
+        self.diff_scale = Scale(
+            menubar,
+            orient=HORIZONTAL,
+            length=150,
+            width=20,
+            from_=0,
+            to=30,
+            resolution=1,
+            tickinterval=5,
+        )
+        self.diff_scale.configure(
+            command=lambda _: self.presenter.on_diff_selected(self.diff_scale.get())
+        )
+        self.diff_scale.pack(side=LEFT)
+        # Filler Radio Buttons < Menu Bar
+        _filler_choice = IntVar(self, 0)
+        use_normalfiller_rb = Radiobutton(
+            menubar,
+            text="Normal",
+            value=0,
+            variable=_filler_choice,
+            command=lambda: self.presenter.on_normal_filler_selected(),
+        )
+        use_intelligentfiller_rb = Radiobutton(
+            menubar,
+            text="Intelligent",
+            value=1,
+            variable=_filler_choice,
+            command=lambda: self.presenter.on_intelligent_filler_selected(),
+        )
+        use_normalfiller_rb.pack(side=RIGHT)
+        use_intelligentfiller_rb.pack(side=RIGHT)
 
-    def on_open_button_press(self):
+        menubar.pack()
+    
+    def init_presenter(self)->None:
+        self.presenter = Presenter(Path("./models/model1.h5"))
+        self.presenter.set_frontend(self)
+
+    def ask_opening_imagefile(self) -> None:
         fn = filedialog.askopenfilename(filetypes=[("Image", "*")])
         if fn is None or fn == "":
             return
-        image = Image.open(fn)
-        self.set_image(image)
+        self.presenter.on_imagefile_specified(Path(fn))
 
-    def get_color(self) -> tuple:
-        return (
-            int(self.r_input.get() or 0),
-            int(self.g_input.get() or 0),
-            int(self.b_input.get() or 0),
-        )
+    def update_image(self, image: Image.Image) -> None:
+        self.image_canvas.set_image(image)
 
+    def show_message(self, message: str) -> None:
+        messagebox.showinfo("Intelligent Fill Preview", message=message)
 
-class PictureCanvas(Canvas):
-    def __init__(self, master, fill_image):
-        super().__init__(master)
-        self.fill_image = fill_image
-        self.image = None
-        self.image_component_id = None
-        self.canvas_w = None
-        self.canvas_h = None
-        self.display_image_w = None
-        self.display_image_h = None
-        self.ratio = None
-        self.bind("<Button-1>", self.on_click)
-        self.bind("<Configure>", self.on_configure)
+    def ask_fillcolor(self) -> None:
+        hexcolor = convert_color_to_hexcolor(self.presenter.fillcolor)
+        res = colorchooser.askcolor(hexcolor, title="Fill Color")
+        if res[0] is None:
+            return
+        color = Color(r=res[0][0], g=res[0][1], b=res[0][2])
+        self.presenter.on_fillcolor_specified(color)
 
-    def set_image(self, image: Image.Image):
-        self.image = image
-        self.show_image(self.image)
-        self.update_image_size()
+    def update_fillcolor(self, color: Color) -> None:
+        hexcolor = convert_color_to_hexcolor(color)
+        self.fillcolor_button.configure(bg=hexcolor)
 
-    def show_image(self, resized_image: Image.Image):
-        if self.image_component_id:
-            self.delete(self.image_component_id)
-        imagetk = ImageTk.PhotoImage(resized_image)
-        _imagetk_list.append(imagetk)
-        self.image_component_id = self.create_image(
-            resized_image.width / 2, resized_image.height / 2, image=imagetk
-        )
+    def update_diff(self, diff: int) -> None:
+        self.diff_scale.set(diff)
 
-    def update_image_size(self):
-        if self.image and self.canvas_w and self.canvas_h:
-            self.ratio = min(
-                float(self.canvas_w) / float(self.image.width),
-                float(self.canvas_h) / float(self.image.height),
-            )
-            print(self.ratio)
-            self.display_image_w = int(self.image.width * self.ratio)
-            self.display_image_h = int(self.image.height * self.ratio)
-            resized_image = self.image.copy().resize(
-                (self.display_image_w, self.display_image_h)
-            )
-            self.show_image(resized_image)
-
-    def on_configure(self, e):
-        self.canvas_w = e.width
-        self.canvas_h = e.height
-        self.update_image_size()
-
-    def on_click(self, e):
-        x_rate = e.x / self.display_image_w
-        y_rate = e.y / self.display_image_h
-        if (0 <= x_rate <= 1) and (0 <= y_rate <= 1):
-            x = int(self.image.width * x_rate)
-            y = int(self.image.height * y_rate)
-            self.fill_image(self.image, x, y)
-
-
-class MainWindow(Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Intelligent Fill Preview")
-        self.geometry("400x400")
-        self.picture_canvas = PictureCanvas(self, self.fill_image)
-        self.picture_canvas.pack(expand=True, fil=tk.BOTH)
-        self.menu_bar = MenuBar(self, set_image=self.picture_canvas.set_image)
-        self.menu_bar.pack()
-
-    def fill_image(self, image, x, y):
-        self.picture_canvas.set_image(
-            filler.fill(image, x, y, *self.menu_bar.get_color(), 10)
-        )
-
-
-if __name__ == "__main__":
-    main_window = MainWindow()
-    main_window.mainloop()
+    def on_click(self, x: int, y: int) -> None:
+        self.presenter.on_image_click(x, y)
